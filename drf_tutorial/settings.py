bcbcde77d7c6f538cfa54dcedcdce087c9ea0962
Ju Hyeon Do <ji5485@naver.com>
2019-11-27 23:05:01 +0900

    heroku upload

--- drf_tutorial/settings.py
+++ drf_tutorial/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'drf_tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'lowell',
-        'USER': 'name',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
